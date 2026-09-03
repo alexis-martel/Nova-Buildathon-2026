984281f979acc24b7dad8249bdff24ee77827da4
vasya <vasyaignatov85@gmail.com>
2026-09-02 22:12:15 -0400

    Testing feature extraction

--- feature_extraction.py
+++ feature_extraction.py
@@ -2,14 +2,22 @@
 import mne
 mne.viz.set_browser_backend('qt')
 # First test on ANT neuro data set
+freq_bands = {'delta': [0.5, 4], 'theta': [4, 8], 'alpha': [8, 12], 'beta': [12, 30]}
 
 def test_function():
     ant_data = mne.io.read_raw_ant("dataset/ANT_EEG_flipcup/Ewing_Patrick_2026-08-10_13-07-25_session-01.cnt")
-    freq_bands = {'delta': [0.5, 4], 'theta': [4, 8], 'alpha': [8, 12], 'beta': [12, 30]}
+    
+    #Crop the data based on the start and end events
+    START_CODE = "64"
+    END_CODE = "128"
+    start_time = ant_data.annotations.onset[ant_data.annotations.description == START_CODE]
+    end_time = ant_data.annotations.onset[ant_data.annotations.description == END_CODE]
+    ant_data_cropped = ant_data.copy().crop(tmin=start_time[0], tmax=end_time[0])
+
     frontal_channels = ['Fp1', 'Fp2', 'F3', 'F4', 'F7', 'F8', 'Fz']
-    frontal_data = ant_data.copy().pick(frontal_channels)
+    frontal_data = ant_data_cropped.copy().pick(frontal_channels)
     spectrum = frontal_data.compute_psd(fmin=4, fmax=8)
-    spectrum.plot(average=False)
-    input("Press Enter to close the plot and exit the script...")
+    #spectrum.plot(average=False)
+    #input("Press Enter to close the plot and exit the script...")
 
 test_function()
